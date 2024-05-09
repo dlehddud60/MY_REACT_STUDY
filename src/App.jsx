73c7a759f4595f66cf238c71f633c9ad9da14bf0
dlehddud60 <dlehddud60@naver.com>
2024-05-09 11:01:28 +0900

    컴포넌트 설명, 주석 다는 방법

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+function MyComponent() {
+  // return: 화면에 출력할 코드 작성
+  return <div>hello component</div>;
+}
+
+function MyComp() {
+  const myElem = <h1>hi JSX</h1>;
+
+  return myElem;
+}
+
 function App() {
   return (
     <>
       <div>
-        <h1>hello react</h1>
+        {/*주석*/}
+        <h1>
+          hello
+          <br />
+          react
+        </h1>
+        <h2>하이 리액트</h2>
+        <h3>안녕 리액트</h3>
+        <MyComponent />
+        <MyComp />
       </div>
     </>
   );
