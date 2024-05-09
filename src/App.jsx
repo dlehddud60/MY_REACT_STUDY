c2a6e65b19103fb7c3da813378d1cc011d0732ee
dlehddud60 <dlehddud60@naver.com>
2024-05-09 14:15:58 +0900

    ||연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 function App() {
-  return <div></div>;
+  const name = "흥민";
+  const age = 21;
+
+  return (
+    <div>
+      <p>{name}</p>
+      {name === "흥민" && <p>공격수</p>}
+      {name === "강인" && <p>미드필더</p>}
+      {age >= 20 && <p>투표 가능</p>}
+      {age < 20 && <p>투표 불가능</p>}
+      {age < 20 || <p>투표 가능</p>}
+      {age >= 20 || <p>투표 불가능</p>}
+    </div>
+  );
 }
 
 export default App;
