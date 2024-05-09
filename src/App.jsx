233ad423487c805c9412306a4251ccb410935563
dlehddud60 <dlehddud60@naver.com>
2024-05-09 12:27:41 +0900

    jsx 내에서 {}로 자바스크립트 코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 function App() {
-  return <div></div>;
+  const val1 = 5;
+  const val2 = 9;
+
+  function someMethod() {
+    return "hello js";
+  }
+
+  function someMethod2(react) {
+    return (
+      <p>
+        {react} {react}
+      </p>
+    );
+  }
+
+  return (
+    <p>
+      <p>
+        {val1} X {val2} = {val1 * val2}
+      </p>
+      <p>{someMethod()}</p>
+      <p>{someMethod2("react")}</p>
+    </p>
+  );
 }
 
 export default App;
