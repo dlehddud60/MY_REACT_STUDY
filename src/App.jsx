60e4aab750fe361408d130e5463c981df86c1836
dlehddud60 <dlehddud60@naver.com>
2024-05-09 11:37:49 +0900

    최상위 컴포넌트 1개, Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+function MyHeader() {
+  return (
+    <div>
+      <h1>hello header</h1>
+      <h3>hello sub eader</h3>
+    </div>
+  );
+}
+function MyFooter() {
+  return (
+    <ul>
+      <li>link1</li>
+      <li>link2</li>
+    </ul>
+  );
+}
+function MyNav() {
+  return (
+    <>
+      <a href="#">link 1</a>
+      <a href="#">link 2</a>
+    </>
+  );
+}
 function App() {
   return (
     <>
       <div>
         <h1>hello react</h1>
+        <MyHeader />
+        <MyFooter />
+        <MyNav />
       </div>
     </>
   );
