28c8e02c557265d36ea2a871fa911b9d5abb9c38
dlehddud60 <dlehddud60@naver.com>
2024-05-09 14:07:43 +0900

    코드 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,5 @@
 function App() {
-  const v1 = 5;
-  const v2 = 7;
-  function someAction() {
-    return "흥민";
-  }
-  function someAction2(p) {
-    return p * p;
-  }
-
-  return (
-    <>
-      <p>
-        {2} + {3} = {2 + 3}
-      </p>
-      <p>
-        {v1} + {v2} = {v1 + v2}
-      </p>
-      <p>
-        {v1} - {v2} = {v1 - v2}
-      </p>
-      <p>{someAction()}</p>
-      <p>{someAction2(5)}</p>
-    </>
-  );
+  return;
 }
 
 export default App;
