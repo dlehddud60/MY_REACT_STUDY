0a7d0d429443953ea56c8f36a151840e71770818
dlehddud60 <dlehddud60@naver.com>
2024-05-09 11:38:53 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,5 @@
-function MyComponent() {
-  // return: 화면에 출력할 코드 작성
-  return <div>hello component</div>;
-}
-
-function MyComp() {
-  const myElem = <h1>hi JSX</h1>;
-
-  return myElem;
-}
-
 function App() {
-  return (
-    <>
-      <div>
-        {/*주석*/}
-        <h1>
-          hello
-          <br />
-          react
-        </h1>
-        <h2>하이 리액트</h2>
-        <h3>안녕 리액트</h3>
-        <MyComponent />
-        <MyComp />
-      </div>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
