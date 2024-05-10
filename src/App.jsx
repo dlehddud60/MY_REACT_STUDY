875c7037db50a93d8ef79f2e5d072d407279c985
dlehddud60 <dlehddud60@naver.com>
2024-05-10 15:02:56 +0900

    컴포넌트 mybutton만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+function Mybutton(props) {
+  return (
+    <div>
+      <button>{props.name}</button>
+    </div>
+  );
+}
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Mybutton name="버튼1"></Mybutton>
+      <Mybutton name="클릭"></Mybutton>
+    </div>
+  );
 }
 
 export default App;
